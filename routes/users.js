19eb02e4462d09b78c257251c08f18e61fd4b770
Chayan <chayanbansal1999@gmail.com>
2019-08-31 20:06:06 +0530

    inser user registration data

--- routes/users.js
+++ routes/users.js
@@ -7,26 +7,14 @@ const MongoClient = mongodb.MongoClient;
 var url = "mongodb://localhost:27017/socomodb";
 
 //add user data
-function insertUser(){
+function insertUser(newUser){
     //now here return a promise
     return new Promise((resolve, reject) => {
 MongoClient.connect(url, function(err, db) {
   if (err) reject(err);
   var dbo = db.db("socomudb");
-  var newUser = { 
-      user_id: "j",
-      name:"ch",
-      login_password:"ghn",
-      artists:['g','j'],
-      genres:['d','l'],
-      languages:['h','l'],
-      dob:"13/7/199",
-      email_id:"dhdh@hjh",
-      communities:['k','k']
-    };
   dbo.collection("users").insertOne(newUser, function(err, res) {
     if (err) throw err;
-    console.log("1 document inserted");
     db.close();
     resolve()
   });
@@ -35,8 +23,14 @@ MongoClient.connect(url, function(err, db) {
 }
 
 //index
-router.get("/",async (req, res) => {
-    await insertUser()
+router.post("/add",async (req, res) => {
+  var newUser = { 
+    name:req.body.name,
+    login_password:req.body.password,
+    dob:req.body.dob,
+    email_id:req.body.email
+  };
+    await insertUser(newUser)
   res.render("users/index");
 });
 
